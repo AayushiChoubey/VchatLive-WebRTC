b8c918cc293c0e1faa875241a72d0d080add68b6
Aayushi <ee200002001@iiti.ac.in>
2023-03-18 23:24:55 +0530

    Update main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+let localStream;
+let remoteStream;
+
+let init = async () => {
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    document.getElementById('user-1').srcObject = localStream;
+}
+
+init();
