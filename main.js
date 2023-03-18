b50674e8e6c3168ccd1a16edeaed393f26ef7019
Aayushi <ee200002001@iiti.ac.in>
2023-03-19 00:10:46 +0530

    icecandidate and servers

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ let localStream;
 let remoteStream;
 let peerConnection;
 
+const servers = {
+    iceServers: [
+        {
+            urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+
+        }
+    ]
+};
+
 let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
     document.getElementById('user-1').srcObject = localStream;
@@ -14,6 +23,23 @@ let createOffer = async () => {
 
     remoteStream = new MediaStream();
     document.getElementById('user-2').srcObject = remoteStream;
+
+    localStream.getTracks().forEach(track => {
+        peerConnection.addTrack(track, localStream);
+    });
+
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach(track => {
+            remoteStream.addTrack(track);
+        });
+    }
+
+    peerConnection.onicecandidate = (event) => {
+        if (event.candidate) {
+            console.log("New ICE Candidate :", event.candidate);
+        }
+    }
+
 
     let offer= await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
