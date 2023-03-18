2953f670dc14177f141e7268df272a00360f0637
Aayushi <ee200002001@iiti.ac.in>
2023-03-18 23:45:58 +0530

    peerConnection createOffer

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 let localStream;
 let remoteStream;
+let peerConnection;
 
 let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
     document.getElementById('user-1').srcObject = localStream;
+
+    createOffer();
 }
+
+let createOffer = async () => {
+    peerConnection = new RTCPeerConnection();
+
+    remoteStream = new MediaStream();
+    document.getElementById('user-2').srcObject = remoteStream;
+
+    let offer= await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    console .log("Offer :",offer);
+
+}
+
 
 init();
