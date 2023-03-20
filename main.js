bb18d96921caafc4a6b523591e64f5a07edccd1a
Aayushi <ee200002001@iiti.ac.in>
2023-03-21 00:19:38 +0530

    createAnswer

--- main.js
+++ main.js
@@ -38,9 +38,21 @@ let init = async () => {
 }
 
 
-let handleMessageFromPeer = async (message, peerId) => {
+let handleMessageFromPeer = async (message, MemberId) => {
     message = JSON.parse(message.text);
-    console.log("Message From Peer :", message);
+    if(message.type === 'offer'){
+        createAnswer(MemberId, message.offer)
+    }
+
+    if(message.type === 'answer'){
+        addAnswer(message.answer)
+    }
+
+    if(message.type === 'candidate'){
+        if(peerConnection){
+            peerConnection.addIceCandidate(message.candidate)
+        }
+    }
 }
 
 let handleUserJoined = async (MemberId) => {
@@ -49,40 +61,65 @@ let handleUserJoined = async (MemberId) => {
 } 
 
 
+let createPeerConnection = async (MemberId) => {
 
-let createOffer = async (MemberId) => {
-    peerConnection = new RTCPeerConnection();
+    peerConnection = new RTCPeerConnection(servers)
 
-    remoteStream = new MediaStream();
-    document.getElementById('user-2').srcObject = remoteStream;
+    remoteStream = new MediaStream()
+    document.getElementById('user-2').srcObject = remoteStream
+    document.getElementById('user-2').style.display = 'block'
 
-    if(!localStream) {
-        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-        document.getElementById('user-1').srcObject = localStream;
+    document.getElementById('user-1').classList.add('smallFrame')
+
+
+    if(!localStream){
+        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+        document.getElementById('user-1').srcObject = localStream
     }
 
-    localStream.getTracks().forEach(track => {
-        peerConnection.addTrack(track, localStream);
-    });
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream)
+    })
 
     peerConnection.ontrack = (event) => {
-        event.streams[0].getTracks().forEach(track => {
-            remoteStream.addTrack(track);
-        });
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack(track)
+        })
     }
 
-    peerConnection.onicecandidate = (event) => {
-        if (event.candidate) {
-            client.sendMessageToPeer({text:JSON.stringify({'type':'candidate', 'candidate':event.candidate})}, MemberId);
+    peerConnection.onicecandidate = async (event) => {
+        if(event.candidate){
+            client.sendMessageToPeer({text:JSON.stringify({'type':'candidate', 'candidate':event.candidate})}, MemberId)
         }
     }
+}
 
 
+let createOffer = async (MemberId) => {
+   
+
+    await createPeerConnection(MemberId)
     let offer= await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
 
     client.sendMessageToPeer({text:JSON.stringify({'type':'offer', 'offer':offer})}, MemberId);
 }
 
+let createAnswer = async (MemberId, offer) => {
+    await createPeerConnection(MemberId)
+
+    await peerConnection.setRemoteDescription(offer)
+
+    let answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+
+    client.sendMessageToPeer({text:JSON.stringify({'type':'answer', 'answer':answer})}, MemberId)
+}
+
+let addAnswer = async (answer) => {
+    if(!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer)
+    }
+}
 
 init();
